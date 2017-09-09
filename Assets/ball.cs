8b06b226669f10b938cb6b8f7495d4f9100c3662
T Sai Ruthvik <f2013489@goa.bits-pilani.ac.in>
2017-09-10 02:20:02 +0530

    AR Game
    The famous Ball ina Maze Puzzle in Augmented Reality Version

---
+++ Assets/ball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+	public GameObject plane;
+	public GameObject spawn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (transform.position.y < plane.transform.position.y - 4){
+			transform.position = spawn.transform.position;
+		}
+		
+	}
+}
